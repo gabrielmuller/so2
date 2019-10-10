7062fc609deb932b756a01bcecb9f868b9a5bfd0
Gabriel Müller <gabrielmullerw@gmail.com>
2019-10-10 15:59:55 -0300

    Files separated

--- epos-ine5424/include/machine/pc/rtl8139.h
+++ epos-ine5424/include/machine/pc/rtl8139.h
@@ -49,6 +49,20 @@ private:
     typedef MMU::DMA_Buffer DMA_Buffer;
     typedef Ethernet::Address MAC_Address;
 
+    // PCI ID
+    static const unsigned int PCI_VENDOR_ID = 0x10EC;
+    static const unsigned int PCI_DEVICE_ID = 0x8139; // RTL8139
+    static const unsigned int PCI_REG_IO = 0;
+    static const unsigned int UNITS = Traits<RTL8139>::UNITS;
+
+    // Buffer config
+    static const unsigned int RX_BUFFER_SIZE = (8192 + 16 + 1500 + 3) & ~3;
+    static const unsigned int TX_BUFFER_SIZE = (sizeof(Frame) + 3) & ~3;
+    static const unsigned int TX_BUFFER_NR = 4;
+
+    // Size of the DMA Buffer
+    static const unsigned int DMA_BUFFER_SIZE = RX_BUFFER_SIZE + TX_BUFFER_SIZE * 4;
+
 public:
     ~RTL8139();
 
@@ -58,41 +72,59 @@ public:
     Buffer * alloc(const Address & dst, const Protocol & prot, unsigned int once, unsigned int always, unsigned int payload);
     int send(Buffer * buf);
     void free(Buffer * buf);
-    bool drop(unsigned int id);
 
-    const Address & address();
-    void address(const Address &);
+    const Address & address() { return _address; }
+    void address(const Address & address) { _address = address; }
 
-    unsigned int period();
-
-    const Statistics & statistics();
+    const Statistics & statistics() { return _statistics; }
 
     void reset();
 
-    static RTL8139 * get(unsigned int unit = 0) { return _device.device; }
+    static RTL8139 * get(unsigned int unit = 0) { return _devices[unit].device; }
 
 protected:
     RTL8139(unsigned int unit, IO_Port io_port, IO_Irq irq, DMA_Buffer * dma_buf);
 
 private:
+    unsigned int _unit;
+
     static void init(unsigned int unit);
 
     void handle_int();
+
+    static void int_handler(const IC::Interrupt_Id & interrupt);
+
+    static RTL8139 * get_by_interrupt(unsigned int interrupt) {
+        for(unsigned int i = 0; i < UNITS; i++)
+            if(_devices[i].interrupt == interrupt)
+                return _devices[i].device;
+        db<RTL8139>(WRN) << "RTL8139::get_by_interrupt(" << interrupt << ") => no device bound!" << endl;
+        return 0;
+    };
 
     struct Device {
         RTL8139 * device;
         unsigned int interrupt;
     };
 
-    static Device _device;
-};
+    static Device _devices[UNITS];
 
-// inline RTL8139::Timer::Time_Stamp RTL8139::Timer::Timer::read() { return TSC::time_stamp(); }
-// inline void RTL8139::Timer::adjust(const RTL8139::Timer::Offset & o) { TSC::time_stamp(o); }
-// inline RTL8139::Timer::Hertz RTL8139::Timer::frequency() { return TSC::frequency(); }
-// inline RTL8139::Timer::PPB RTL8139::Timer::accuracy() { return TSC::accuracy(); }
-// inline RTL8139::Timer::Time_Stamp RTL8139::Timer::us2count(const RTL8139::Timer::Microsecond & us) { return Convert::us2count<Time_Stamp, Microsecond>(TSC::frequency(), us); }
-// inline RTL8139::Timer::Microsecond RTL8139::Timer::count2us(const RTL8139::Timer::Time_Stamp & ts) { return Convert::count2us<Hertz, Time_Stamp, Microsecond>(TSC::frequency(), ts); }
+    Statistics _statistics;
+    Address _address;
+    IO_Irq _irq;
+    DMA_Buffer * _dma_buf;
+    IO_Port _io_port;
+
+    Buffer * _rx_buffer;
+    Buffer * _tx_buffer[TX_BUFFER_NR];
+
+
+    volatile unsigned char _tx_tail = 0; // What descriptor is the NIC using?
+    unsigned char _tx_head = 0;          // What descriptor is the CPU using?
+
+    char * _tx_base_phy[TX_BUFFER_NR];
+    char * _rx_base_phy;
+};
 
 __END_SYS
 
--- epos-ine5424/src/machine/pc/rtl8139.cc
+++ epos-ine5424/src/machine/pc/rtl8139.cc
@@ -8,7 +8,7 @@
 __BEGIN_SYS
 
 // Class attributes
-RTL8139::Device RTL8139::_device;
+RTL8139::Device RTL8139::_devices[UNITS];
 
 
 // Methods
@@ -19,20 +19,25 @@ RTL8139::~RTL8139()
 
 
 int RTL8139::send(const Address & dst, const Protocol & prot, const void * data, unsigned int size)
-{
-    new (_tx_base[_tx_head]) Frame(_address, dst, prot, data, size);
-    unsigned short status = sizeof(Frame) & 0xfff;
+{   
+    db<RTL8139>(WRN) << "RTL8139 sending " << _tx_head << endl;
 
-    while (tx_is_using(_tx_head)) {
-        // How to remove this busy wait?
-    }
-    tx_use(_tx_head);
+    Buffer * buf = _tx_buffer[_tx_head];
+
+    while (!buf->lock());
+
+    db<RTL8139>(WRN) << "RTL8139 " << _tx_head << " locked" << endl;
+
+    new (buf->frame()) Frame(_address, dst, prot, data, size);
+    unsigned short status = sizeof(Frame) & 0xfff;
 
     CPU::out32(_io_port + TRSTART  + _tx_head * 4, (long unsigned int) _tx_base_phy[_tx_head]);
     CPU::out32(_io_port + TRSTATUS + _tx_head * 4, status);
 
     _tx_head = (_tx_head + 1) % TX_BUFFER_NR;
-    db<RTL8139>(WRN) << dst << endl;
+
+    // db<RTL8139>(WRN) << "RTL8139::send " << dst << endl;
+    db<RTL8139>(WRN) << "RTL8139 sent, _tx_head now at " << _tx_head << endl;
 
     return size;
 }
@@ -47,39 +52,7 @@ int RTL8139::receive(Address * src, Protocol * prot, void * data, unsigned int s
 // Allocated buffers must be sent or release IN ORDER as assumed by the RTL8139
 RTL8139::Buffer * RTL8139::alloc(const Address & dst, const Protocol & prot, unsigned int once, unsigned int always, unsigned int payload)
 {
-    db<RTL8139>(TRC) << "RTL8139::alloc(s=" << _address << ",d=" << dst << ",p=" << hex << prot << dec << ",on=" << once << ",al=" << always << ",pl=" << payload << ")" << endl;
-
-    int max_data = MTU - always;
-
-    if((payload + once) / max_data > TX_BUFS) {
-        db<RTL8139>(WRN) << "RTL8139::alloc: sizeof(Network::Packet::Data) > sizeof(NIC::Frame::Data) * TX_BUFS!" << endl;
-        return 0;
-    }
-
-    Buffer::List pool;
-
-    // Calculate how many frames are needed to hold the transport PDU and allocate enough buffers
-    for(int size = once + payload; size > 0; size -= max_data) {
-        // Wait for the next buffer to become free and seize it
-        unsigned int i = _tx_cur;
-        for(bool locked = false; !locked; ) {
-            for(; _tx_ring[i].status & Tx_Desc::OWN; ++i %= TX_BUFS);
-            locked = _tx_buffer[i]->lock();
-        }
-        _tx_cur = (i + 1) % TX_BUFS;
-        Tx_Desc * desc = &_tx_ring[i];
-        Buffer * buf = _tx_buffer[i];
-
-        // Initialize the buffer and assemble the Ethernet Frame Header
-        new (buf) Buffer(this, (size > max_data) ? MTU : size + always, _address, dst, prot);
-
-        db<RTL8139>(INF) << "RTL8139::alloc:desc[" << i << "]=" << desc << " => " << *desc << endl;
-        db<RTL8139>(INF) << "RTL8139::alloc:buf=" << buf << " => " << *buf << endl;
-
-        pool.insert(buf->link());
-    }
-
-    return pool.head()->object();
+    return nullptr;
 }
 
 
@@ -89,29 +62,7 @@ int RTL8139::send(Buffer * buf)
 }
 
 
-void RTL8139::free(Buffer * buf)
-{
-    db<RTL8139>(TRC) << "RTL8139::free(buf=" << buf << ")" << endl;
-
-    db<RTL8139>(INF) << "RTL8139::free:buf=" << buf << " => " << *buf << endl;
-
-    for(Buffer::Element * el = buf->link(); el; el = el->next()) {
-        buf = el->object();
-        Rx_Desc * desc = reinterpret_cast<Rx_Desc *>(buf->back());
-
-        _statistics.rx_packets++;
-        _statistics.rx_bytes += buf->size();
-
-        // Release the buffer to the NIC
-        desc->size = Reg16(-sizeof(Frame)); // 2's comp.
-        desc->status = Rx_Desc::OWN; // Owned by NIC
-
-        // Release the buffer to the OS
-        buf->unlock();
-
-        db<RTL8139>(INF) << "RTL8139::free:desc=" << desc << " => " << *desc << endl;
-    }
-}
+void RTL8139::free(Buffer * buf) {}
 
 
 void RTL8139::reset()
@@ -125,7 +76,11 @@ void RTL8139::reset()
     db<RTL8139>(WRN) << "DMA Buffer is at phy " << _dma_buf->phy_address() << endl;
 
     // Reset the device
-    s_reset();
+
+    CPU::out8(_io_port + CMD, RESET);
+
+    // Wait for RST bit to be low
+    while (CPU::in8(_io_port + CMD) & RESET) {}
 
     // Tell the NIC where the receive buffer starts
     CPU::out32(_io_port + RBSTART, _dma_buf->phy_address());
@@ -142,26 +97,11 @@ void RTL8139::reset()
     // Set MAC address
     for (int i = 0; i < 6; i++) _address[i] = CPU::in8(_io_port + MAC0_5 + i);
 
-    const char msg[] = "Frame frame frame hellooooo";
-    for (int i = 0; i < 1000; i++) send(0xbabaca + i, 0x0806, msg, sizeof(msg));
+    //const char msg[] = "Frame frame frame hellooooo";
+    //for (int i = 0; i < 4; i++) send(0xbabaca + i, 0x0806, msg, sizeof(msg));
 
     db<RTL8139>(WRN) << "RBSTART is " << CPU::in32(_io_port + RBSTART) << endl;
 
-}
-
-void RTL8139::tx_use(unsigned char index)
-{
-    _tx_busy |= 1 << index;
-}
-
-void RTL8139::tx_release(unsigned char index)
-{
-    _tx_busy &= ~(1 << index);
-}
-
-volatile bool RTL8139::tx_is_using(unsigned char index)
-{
-    return (_tx_busy & (1 << index)) != 0;
 }
 
 void RTL8139::handle_int()
@@ -177,9 +117,9 @@ void RTL8139::handle_int()
         db<RTL8139>(WRN) << "TOK" << endl;
         for (unsigned char i = 0; i < TX_BUFFER_NR; i++) {
             // While descriptors have TOK status, release and advance tail
-            if (tx_is_using(_tx_tail) &&
-                CPU::in32(_io_port + TRSTATUS + _tx_tail * 4) & STATUS_TOK) {
-                tx_release(_tx_tail);
+            if (CPU::in32(_io_port + TRSTATUS + _tx_tail * 4) & STATUS_TOK) {
+                _tx_buffer[_tx_tail]->unlock();
+                db<RTL8139>(WRN) << "TOK unlocked " << _tx_tail << endl;
                 _tx_tail = (_tx_tail + 1) % TX_BUFFER_NR;
             } else break;
         }
@@ -198,7 +138,7 @@ void RTL8139::handle_int()
 
 void RTL8139::int_handler(const IC::Interrupt_Id & interrupt)
 {
-    RTL8139 * dev = _device;
+    RTL8139 * dev = get_by_interrupt(interrupt);
 
     db<RTL8139>(WRN) << "RTL8139::int_handler(int=" << interrupt << ",dev=" << dev << ")" << endl;
 
