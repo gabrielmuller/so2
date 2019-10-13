c73fe29d2d83db32e2dc33eec4de7b13cd0ae29d
Juliana Pinheiro <julianas.pinheiro@outlook.com>
2019-10-13 19:43:23 -0300

    DMA test working

--- epos-ine5424/app/dma_test.cc
+++ epos-ine5424/app/dma_test.cc
@@ -1,60 +1,114 @@
 // EPOS DMA Test Program
 
+// Remember to set Traits<Build>::NODES = 2 in dma_test_traits.h
+
 #include <machine/nic.h>
+#include <netservice.h>
+#include <process.h>
 #include <time.h>
+
+#define FIBBONACCI_N 40
 
 using namespace EPOS;
 
 OStream cout;
 
+Thread *a;
+Thread *b;
+
 int fibbonacci(int n) {
-   if(n == 0){
+   if (n == 0) {
       return 0;
-   } else if(n == 1) {
+   } else if (n == 1) {
       return 1;
    } else {
-      return (fibbonacci(n-1) + fibbonacci(n-2));
+      return fibbonacci(n - 1) + fibbonacci(n - 2);
    }
 }
 
-int main()
-{
-    cout << "DMA Test" << endl;
+int cpubound(void) {
+   fibbonacci(FIBBONACCI_N);
+}
 
-    cout << "\nStarting CPU-bound operations..." << endl;
+int sendrecv_data(void) {
+   NetService::start();
 
-    Chronometer chrono;
-    chrono.start();
-    
-    int a = fibbonacci(30);
+   NIC<Ethernet> *nic = Traits<Ethernet>::DEVICES::Get<0>::Result::get(0);
+   NIC<Ethernet>::Address src, dst;
+   NIC<Ethernet>::Protocol prot;
+   char data[nic->mtu()];
 
-    chrono.stop();
+   NIC<Ethernet>::Address self = nic->address();
 
-    cout << "\nEnding CPU-bound operations... fibbo(n=30) = " << a 
-         << "\nThe measured time was " << chrono.read() / 1000 <<" ms!"
-         << "\nStarting DMA operations..." << endl;
+   if (self[5] % 2) { // sender
+      Delay(5000000);
 
-    chrono.reset();
-    chrono.start();
-    
-    // dma
+      memset(data, '0', nic->mtu());
+      data[nic->mtu() - 1] = '\n';
+      NetService::send(nic->broadcast(), 0x8888, data, nic->mtu());
+   } else {
+      NetService::receive(&src, &prot, data, nic->mtu());
+      // cout << "  Data: " << data;
+   }
 
-    chrono.stop();
+   // NIC<Ethernet>::Statistics stat = nic->statistics();
+   // cout << "Statistics\n"
+   //      << "Tx Packets: " << stat.tx_packets << "\n"
+   //      << "Tx Bytes:   " << stat.tx_bytes << "\n"
+   //      << "Rx Packets: " << stat.rx_packets << "\n"
+   //      << "Rx Bytes:   " << stat.rx_bytes << "\n";
+}
 
-    cout << "\nEnding DMA operations..."
-         << "\nThe measured time was " << chrono.read() / 1000 <<" ms!" << endl;
+int main() {
+   int cpu_time = 0, dma_time = 0, both_time = 0;
 
-    return;
+   cout << "DMA Test" << endl;
 
-    cout << "\nStarting DMA and CPU-bound operations..." << endl;
+   cout << "\nStarting CPU-bound operations..." << endl;
 
-    chrono.reset();
-    chrono.start();
-    
-    // dma + fibo
+   Chronometer chrono;
+   chrono.start();
 
-    chrono.stop();
+   int fibo = fibbonacci(40);
 
-    cout << "\nEnding DMA and CPU-bound operations..."
-         << "\nThe measured time was " << chrono.read() / 1000 <<" ms!" << endl;
+   chrono.stop();
+   cpu_time = chrono.read() / 1000;
+
+   cout << "\nEnding CPU-bound operations... fibbo(n=" << FIBBONACCI_N << ") = " << fibo
+        << "\nThe measured time was " << cpu_time << " ms!"
+        << "\nStarting DMA operations..." << endl;
+
+   chrono.reset();
+   chrono.start();
+
+   sendrecv_data();
+
+   chrono.stop();
+   dma_time = chrono.read() / 1000;
+
+   cout << "\nEnding DMA operations..."
+        << "\nThe measured time was " << dma_time << " ms!"
+        << "\nStarting DMA and CPU-bound operations..." << endl;
+
+   chrono.reset();
+   chrono.start();
+
+   a = new Thread(&sendrecv_data);
+   b = new Thread(&cpubound);
+   a->join();
+   b->join();
+
+   chrono.stop();
+   both_time = chrono.read() / 1000;
+
+   cout << "\nEnding DMA and CPU-bound operations..."
+        << "\nThe measured time was " << both_time << " ms!" << endl;
+
+   cout << "DMA Test Results:\n"
+        << "CPU-bound operations: " << cpu_time  << "ms\n"
+        << "DMA operations:       " << dma_time  << "ms\n"
+        << "DMA+CPU operations:   " << both_time << "ms\n";
+
+   delete a;
+   delete b;
 }
